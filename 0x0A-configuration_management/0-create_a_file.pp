3785f935432c4176c8870cf19b91df4cd0a56720
Mercy Aupat <89969949+maupat552@users.noreply.github.com>
2023-01-12 08:11:23 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# This manifest creates a file at /tmp
+# creates a file in /tmp
+
 file { '/tmp/school':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
